ebea23cc52dc48d11473e1038f5eefe2027dd12b
d.baranov <d.baranov@platformaofd.ru>
2023-03-11 11:10:37 +0700

    initial commit

---
+++ DS-Arrays/Arrays.js
@@ -0,0 +1,49 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index){
+    return this.data[index];
+  }
+
+  push(item){
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop(){
+    const lastItem = this.data[this.length];
+    delete this.data[this.length-1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index){
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+
+  shiftItems(index){
+    console.log('shift item index:' + index + ' item:' + this.data[index]);
+    for (var i = index; i < this.length-1; i++) {
+      this.data[i] = this.data[i+1];
+    }
+    this.length--;
+  }
+}
+
+const array = new MyArray();
+array.push("hi");
+array.push("you");
+array.push("!");
+array.pop();
+array.push('hello');
+array.push('world');
+array.push('!');
+array.delete(2);
+console.log(array);
+
